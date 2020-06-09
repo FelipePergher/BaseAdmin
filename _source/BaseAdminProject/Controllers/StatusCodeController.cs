a854ead1bb0532c01fdf2d4abb30a570bdec2688
Felipe Pergher <felipepergher_10@hotmail.com>
2020-06-09 00:00:40 -0300

    #477 added status code pages to project (400 and 500)

---
+++ _source/BaseAdminProject/Controllers/StatusCodeController.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="StatusCodeController.cs" company="Felipe Pergher">
+// Copyright (c) Felipe Pergher. All Rights Reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BaseAdminProject.Controllers
+{
+    [Authorize]
+    public class StatusCodeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public StatusCodeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index(int code)
+        {
+            switch (code)
+            {
+                case 404:
+                    return View("404");
+                case 500:
+                    return View("500");
+                default:
+                    return View("500");
+            }
+        }
+    }
+}
--- _source/BaseAdminProject/Startup.cs
+++ _source/BaseAdminProject/Startup.cs
@@ -91,7 +91,7 @@ namespace BaseAdminProject
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/StatusCode", "?code={0}");
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
